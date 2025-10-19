fe7b12ad79cde6eb603c723a13a671edbb69ee64
Madeleine Negård <madelne@stud.ntnu.no>
2025-10-19 19:05:28 +0200

    Add + and > operators

--- Oving8/oppgave2.hpp
+++ Oving8/oppgave2.hpp
@@ -1,20 +1,23 @@
 
-template <typename Type>
+template <typename Type1, typename Type2>
 
 class Pair{
   public:
-    Type a;
-    Type b;
-    Pair(Type a, Type b){
-      a = a;
-      b = b;
+    Type1 first;
+    Type2 second;
+    Pair(Type1 a, Type2 b){
+      first = a;
+      second = b;
     }
 
-    Pair operator+(const Type a){
-
+    Pair operator+(const Pair &pair){
+      Pair pair2 = *this;
+      pair2.first += pair.first;
+      pair2.second += pair.second;
+      return pair2;
     }
 
-    Pair operator<(const Pair pair){
-
-    }
+    bool operator>(const Pair &pair) const{
+        return (this->first + this->second) > (pair.first + pair.second);
+      }
 };

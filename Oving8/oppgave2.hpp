15cebe66af013bc746094276db5cf9a2ee67d1fe
Madeleine Negård <madelne@stud.ntnu.no>
2025-10-19 18:48:26 +0200

    Pair class constructor

---
+++ Oving8/oppgave2.hpp
@@ -0,0 +1,20 @@
+
+template <typename Type>
+
+class Pair{
+  public:
+    Type a;
+    Type b;
+    Pair(Type a, Type b){
+      a = a;
+      b = b;
+    }
+
+    Pair operator+(const Type a){
+
+    }
+
+    Pair operator<(const Pair pair){
+
+    }
+};

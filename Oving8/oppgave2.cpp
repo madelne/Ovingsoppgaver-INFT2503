3b26942015817b9fb415a6eecb7265163d9d4d15
Madeleine Negård <madelne@stud.ntnu.no>
2025-10-19 18:48:45 +0200

    Test Pair class

---
+++ Oving8/oppgave2.cpp
@@ -0,0 +1,14 @@
+nt main() {
+    Pair<double, int> p1(3.5, 14);
+    Pair<double, int> p2(2.1, 7);
+    cout << "p1: " << p1.first << ", " << p1.second << endl;
+    cout << "p2: " << p2.first << ", " << p2.second << endl;
+
+    if (p1 > p2)
+        cout << "p1 er størst" << endl;
+    else
+        cout << "p2 er størst" << endl;
+
+    auto sum = p1 + p2;
+    cout << "Sum: " << sum.first << ", " << sum.second << endl;
+}

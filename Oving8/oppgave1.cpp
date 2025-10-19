3290ab6ccab46fe8331f91baac6dc99c7e39a119
Madeleine Negård <madelne@stud.ntnu.no>
2025-10-19 17:08:46 +0200

    Delete tar files

---
+++ Oving8/oppgave1.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+class Template {
+    template <typename Type>
+
+    bool equal(Type a, Type b) {
+        if (a == b) {
+            return true;
+        }
+        return false;
+    }
+
+    bool equal(double a, double b) {
+        if (abs(a - b) < 0.00001) {
+            return true;
+        }
+        return false;
+    }
+
+
+};
+
+int main() {
+    // Din kode her
+    return 0;
+}
